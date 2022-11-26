1370602a9fb1816194633c77ac7f4e49c766b7af
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2022-11-26 15:11:32 -0600

    Add files via upload

---
+++ red_blue_black_game.py
@@ -0,0 +1,30 @@
+import random, pyfiglet
+
+print(pyfiglet.figlet_format('Pick Colour'))
+
+class Game:
+    def __init__(self):
+        user_wins = 0
+        computer_wins = 0
+        options = ['blue', 'red', 'black', 'green']
+        while True:
+            user_input = input('Enter: Blue/Red/Black/Green OR Enter Q to quit: ').lower()
+            if user_input == 'q':
+                break
+            if user_input not in options:
+                continue
+            random_number = random.randint(0,2)
+            computer_pick = options[random_number]
+            print('computer_pick', computer_pick +'.')
+
+            if computer_pick == user_input:
+                print('You won!!!')
+                user_wins += 1
+            elif user_input != computer_pick:
+                print('You lose')
+                computer_wins +=1
+            else: print('Try again.')
+        print('computer_wins', computer_wins,'times.')
+        print('You_wins', user_wins,'times.')
+        print('\t\tThanks for Playing....')
+game = Game()
